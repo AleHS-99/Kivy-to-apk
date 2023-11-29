2b7b4adbad94f6b5fd43a6814e54ec0a6701f770
Alejandro <84586035+AleHS-99@users.noreply.github.com>
2023-11-29 13:55:39 -0500

    prueba

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
-import bd
 from kivy.utils import QueryDict
 from kivymd.toast import toast
 from kivy.utils import QueryDict, rgba
@@ -36,22 +35,12 @@ class LoginApplication(MDApp):
         return self.manager
 
     def on_start(self):
-        bd.create_database()
         self.manager.current = 'login'
 
     def on_login(self):
         user = self.manager.get_screen('login').ids.user.text
         password = self.manager.get_screen('login').ids.password.text
-        if not bd.has_users():
-            password = 'a12345678.'
-            bd.insert_user(username='admin', password=password)
-            toast("Usuario: admin; Contraseña: admin \n Creado Correctamente", background=[1, 0.5, 0, 1], duration=3)
-        else:
-            user = bd.get_user(user, password)
-            if user:
-                self.manager.current = 'home'
-            else:
-                toast("Usuario y/o Contraseña Incorrectos", background=[1, 0.5, 0, 1], duration=3)
+        self.manager.current = 'home'
 
     def go_dash(self):
         self.manager.current = 'home'
